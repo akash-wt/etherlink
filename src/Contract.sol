8b13bbf6001a8f4cd2d0030aaf1f621d3177b423
akash patel <akash906129@gmail.com>
2025-04-22 01:19:59 +0530

    before openzeplin

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
-contract Contract { }
+contract Contract is ERC20 {
+    constructor() ERC20("Contract", "CT") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
