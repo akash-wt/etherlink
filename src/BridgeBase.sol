99e41961907043aa502f6c9aedecaf686123521c
akash patel <akash906129@gmail.com>
2025-04-23 18:06:53 +0530

    built Bridge

--- src/BridgeBase.sol
+++ src/BridgeBase.sol
@@ -1,16 +1,41 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
-import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
+
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
 
-contract Contract is ERC20, Ownable {
-    constructor(
-        uint256 initialSupply
-    ) ERC20("Gold", "GLD") Ownable(msg.sender) {
-        _mint(msg.sender, initialSupply);
+interface IBGLD is IERC20 {
+    function mint(address _to, uint256 _amount) external;
+    function burn(address _from, uint256 _amount) external;
+}
+
+contract BridgeBase is  Ownable {
+    event Burn(address indexed burner, uint256 amount);
+
+    address public tokenAddress;
+    mapping(address => uint256) public pendingBalance;
+
+    constructor(address _tokenAddress) Ownable(msg.sender) {
+        tokenAddress = _tokenAddress;
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    function burn(uint256 _amount) public {
+        IBGLD token = IBGLD(tokenAddress);
+
+        require(
+            pendingBalance[msg.sender] >= _amount,
+            "Not enough pending balance"
+        );
+        pendingBalance[msg.sender] -= _amount;
+
+        token.burn(msg.sender, _amount);
+        emit Burn(msg.sender, _amount);
+    }
+
+    function burnedOnOppositeChain(
+        address userAccount,
+        uint256 _amount
+    ) public onlyOwner {
+        pendingBalance[userAccount] += _amount;
     }
 }
--- src/BridgeETH.sol
+++ src/BridgeETH.sol
@@ -1,16 +1,53 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
-import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
+
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
 
-contract Contract is ERC20, Ownable {
-    constructor(
-        uint256 initialSupply
-    ) ERC20("Gold", "GLD") Ownable(msg.sender) {
-        _mint(msg.sender, initialSupply);
+contract BridgeETH is Ownable {
+    event Deposit(address indexed depositor, uint256 amount);
+
+    address public tokenAddress;
+    mapping(address => uint256) public pendingBalance;
+
+    constructor(address _tokenAddress) Ownable(msg.sender) {
+        tokenAddress = _tokenAddress;
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    function deposite(uint256 _amount) public {
+        IERC20 token = IERC20(tokenAddress);
+
+        require(
+            token.allowance(msg.sender, address(this)) >= _amount,
+            "Allowance too low"
+        );
+        require(
+            token.transferFrom(msg.sender, address(this), _amount),
+            "Transfer failed"
+        );
+
+        emit Deposit(msg.sender, _amount);
+    }
+
+    function withdraw(uint256 _amount) public {
+        IERC20 token = IERC20(tokenAddress);
+        require(
+            pendingBalance[msg.sender] >= _amount,
+            "Insufficient pending balance"
+        );
+        require(
+            token.balanceOf(address(this)) >= _amount,
+            "Insufficient contract balance"
+        );
+
+        pendingBalance[msg.sender] -= _amount;
+        require(token.transfer(msg.sender, _amount), "Transfer failed");
+    }
+
+    function burnedOnOppositeChain(
+        address userAccount,
+        uint256 _amount
+    ) public onlyOwner {
+        pendingBalance[userAccount] += _amount;
     }
 }
